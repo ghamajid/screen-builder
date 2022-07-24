4e69836e5d893221896a17b967dee67f15182109
Ali Sadat <saasf1381@gmail.com>
2022-07-24 17:40:45 +0430

    Timeline Bug Fixed

--- src/components/renderer/form-timeline.vue
+++ src/components/renderer/form-timeline.vue
@@ -1,74 +1,76 @@
 <template>
-  <div>
-    <p>Change the orientation:</p>
-    <DropDownList
-        :style="{ width: '220px' }"
-        :data-items="['horizontal', 'vertical']"
-        :value="orientation"
-        @change="handleOrientationChange"
-    />
-    <hr/>
-    <Stepper
-        :value="value"
-        @change="handleChange"
-        :items="items"
-        :orientation="orientation"
-    />
-  </div>
+    <div>
+        <!--    <p>Change the orientation:</p>-->
+        <!--    <DropDownList-->
+        <!--        :style="{ width: '220px' }"-->
+        <!--        :data-items="['horizontal', 'vertical']"-->
+        <!--        :value="orientation"-->
+        <!--        @change="handleOrientationChange"-->
+        <!--    />-->
+<!--        <hr/>-->
+        <Stepper
+            :value="value"
+            @change="handleChange"
+            :items="items"
+            :orientation="this.options.renderAs"
+            class="k-rtl"
+        />
+    </div>
 </template>
 
 <script lang="ts">
 import {DropDownList} from '@progress/kendo-vue-dropdowns';
 import {
-  Stepper
+    Stepper
 } from '@progress/kendo-vue-layout';
 
 export default {
-  components: {
-    Stepper,
-    DropDownList
-  },
-  props: [
-    'config',
-    'transientData',
-      'name'
-  ],
-  data() {
-    var name_val = 0;
-    if(this.transientData && this.transientData[this.name]) {
-      name_val = this.transientData[this.name]
-
-    }
-    return {
-      value: name_val,
-      orientation: 'horizontal',
-      items: [
-        {
-          icon: "k-i-lock",
-          label: "Account Info",
-        },
-        {
-          icon: "k-i-user",
-          label: "Personal Info",
-        },
-        {
-          icon: "k-i-myspace",
-          label: "Membership",
-        },
-        {
-          icon: "k-i-track-changes",
-          label: "Verification",
-        },
-      ]
-    }
-  },
-  methods: {
-    handleChange(e) {
-      // this.value = e.value;
+    components: {
+        Stepper,
+        DropDownList
     },
-    handleOrientationChange(e) {
-      this.orientation = e.target.value;
+    props: [
+        'config',
+        'transientData',
+        'name',
+        'options'
+    ],
+    data() {
+        var name_val = 0;
+        if (this.transientData && this.transientData[this.name]) {
+            name_val = this.transientData[this.name]
+            
+        }
+        return {
+            value: name_val,
+            orientation: 'horizontal',
+            items: [
+                {
+                    icon: "k-i-lock",
+                    label: "تایید قوانین",
+                },
+                {
+                    icon: "k-i-user",
+                    label: "اطلاعات هویتی",
+                },
+                {
+                    icon: "k-i-note",
+                    label: "اطلاعات درخواست",
+                },
+                {
+                    icon: "k-i-track-changes",
+                    label: "تایید نهایی",
+                },
+            ]
+        }
+    },
+    methods: {
+        handleChange(e) {
+            // this.value = e.value;
+        },
+        handleOrientationChange(e) {
+            this.orientation = e.target.value;
+        }
     }
-  }
 };
 </script>
