2213f3d931f9249f620a57e02bef31acde7ec1f7
ehsani <a.ehsanizare.3@gmail.com>
2022-08-30 13:33:01 +0430

    hidden field validation

--- src/components/renderer/form-button.vue
+++ src/components/renderer/form-button.vue
@@ -207,50 +207,33 @@ export default {
       }
     },
     fetchItems(items) {
-      let config_name = [];
-      console.log(123121312, items)
       for (const item of items) {
-        // console.log(333, 'item', item['config']['name'])
-
         if (item['config']['name']) {
-          config_name.push(item['config']['name']);
-
           if (this.pageData[item['config']['name']]) {
-            // console.log(555, 'item[config][name]', item['config']['name'], this.pageData[item['config']['name']])
-            console.log('iiii')
-
             this.fetchLoops(this.pageData[item['config']['name']], item);
           }
         }
 
         if (typeof item['items'] !== 'undefined') {
-
           for (const nested_items of item['items']) {
             if (nested_items.length > 0 && Array.isArray(items)) {
-              // console.log(444)
               this.fetchItems(nested_items);
-
             }
           }
-
         }
       }
     },
     fetchLoops(loops, item) {
       let self = this;
-      // console.log('loops,item',loops,item)
       if (loops && loops.$each && typeof loops.$each === 'object') {
         let form_validation = [];
         let form_error = 0;
-        console.log('loops', loops, item)
         Object.keys(loops.$each.$iter).map(function (key1) {
 
           Object.keys(loops.$each.$iter[key1]).forEach(function (key2, index2) {
             if (key2.indexOf('$') == -1) {
               if (item.items) {
-
                 for (const loopItem of item.items) {
-                  console.log(4444, key2, loopItem['config']['name'], typeof loopItem['config']['name'], loopItem)
 
                   if (key2 == loopItem['config']['name']) {
                     // console.log(5555,key2,loops.$each.$iter[key1],loopItem)
@@ -267,8 +250,7 @@ export default {
                     }
                   } else if (typeof loopItem['config']['name'] == 'undefined') {
                     if (loopItem.component === "FormMultiColumn") {
-                      console.log(7777777, loopItem, loopItem.items, key2, loops.$each.$iter[key1])
-                      self.multiColumn(loopItem.items,key2,loops.$each.$iter[key1])
+                      self.multiColumn(loopItem.items, key2, loops.$each.$iter[key1])
 
                     }
                   }
@@ -282,75 +264,38 @@ export default {
         });
         this.validation = this.validation.concat(form_validation);
         this.errors_submit_if_valid += form_error;
-        console.log(666, 'this.validation', this.validation)
-        console.log(666, 'this.errors_submit_if_valid', this.errors_submit_if_valid)
-
-
+       
       } else {
-
-        this.validation.push(!loops.$invalid);
-        if (!loops.$invalid == false) {
-          this.errors_submit_if_valid++;
+        if (item['config']['conditionalHide'] == undefined) {
+          
+          this.validation.push(!loops.$invalid);
+          if (!loops.$invalid == false) {
+            this.errors_submit_if_valid++;
+          }
         }
-        // console.log(777,'this.validation', this.validation,loops.$invalid,item['config']['name'])
-        // console.log(777, 'this.errors_submit_if_valid', this.errors_submit_if_valid)
-
       }
     },
-    multiColumn(columnItems,key2,loops){
-      console.log('multiColumn',columnItems,key2,loops)
+    multiColumn(columnItems, key2, loops) {
       let thic = this;
       for (let tableItems of columnItems) {
         for (let tableItem of tableItems) {
-          // console.log(1212,tableItem['config']['name'] ,tableItem['component'],key2)
           if (tableItem['config']['name'] == key2) {
-            // console.log('tableItem', tableItem, key2)
             thic.fetchColumn(loops, tableItem);
-            // console.log(1313)
 
-          }else if(tableItem['component'] === "FormMultiColumn" ){
-            console.log(9999,'tableItem',tableItem,key2)
-            thic.multiColumn(tableItem.items,key2,loops);
+          } else if (tableItem['component'] === "FormMultiColumn") {
+            thic.multiColumn(tableItem.items, key2, loops);
           }
-
-          //   if (tableItem['config']['name'] == undefined && !form_items.includes(tableItem['config']['name'])){
-          //   console.log(558585858,tableItem)
-          // }
         }
       }
     },
     fetchColumn(table_items, item) {
-      console.log(987987987, table_items, item)
-      // let column_items = [];
-      // console.log(333, 'item', item, item['config']['name'])
 
       if (item['config']['name']) {
-        // column_items.push(item['config']['name']);
 
         if (table_items[item['config']['name']]) {
-          // console.log(555, 'item[config][name]', item['config']['name'], table_items[item['config']['name']])
-          console.log('cccc', table_items[item['config']['name']], item['config']['name'])
           this.fetchLoops(table_items[item['config']['name']], item);
-          // this.validation.push(!table_items[item['config']['name']].$invalid);
-          // if (!table_items[item['config']['name']].$invalid == false) {
-          //   this.errors_submit_if_valid++;
-          // }
-        } else {
-          console.log('00000', item['config']['name'])
         }
       }
-      //
-      // if (typeof item['items'] !== 'undefined'  && column_items.includes(item['config']['name']) === false) {
-      //
-      //   for (const nested_items of item['items']) {
-      //     if (nested_items.length > 0 && Array.isArray(items)) {
-      //       console.log(444)
-      //       this.fetchItems(nested_items);
-      //
-      //     }
-      //   }
-      //
-      // }
     },
   },
 }
