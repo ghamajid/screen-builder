33f23603db4bc5d20bedb7fcc0060438963b9599
ehsani <a.ehsanizare.3@gmail.com>
2022-09-28 14:02:42 +0330

    85

--- src/components/renderer/file-upload.vue
+++ src/components/renderer/file-upload.vue
@@ -36,31 +36,8 @@
         <template>
           <div id="kkkkk">
             <div>
-              <img src="src/assets/loading.svg">
+              <img src="@/assets/loading.svg">
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;background:#fff;display:block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-              <circle cx="80" cy="50" r="5" fill="#d7689d">
-                <animate attributeName="cx" values="80;59.270509831248425" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-                <animate attributeName="cy" values="50;78.53169548885461" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-                <animate attributeName="fill" values="#d7689d;#f3d337" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              </circle><circle cx="59.270509831248425" cy="78.53169548885461" r="5" fill="#f3d337">
-              <animate attributeName="cx" values="59.270509831248425;25.72949016875158" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              <animate attributeName="cy" values="78.53169548885461;67.6335575687742" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              <animate attributeName="fill" values="#f3d337;#a0c443" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-            </circle><circle cx="25.72949016875158" cy="67.6335575687742" r="5" fill="#a0c443">
-              <animate attributeName="cx" values="25.72949016875158;25.729490168751575" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              <animate attributeName="cy" values="67.6335575687742;32.366442431225806" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              <animate attributeName="fill" values="#a0c443;#66b5ae" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-            </circle><circle cx="25.729490168751575" cy="32.366442431225806" r="5" fill="#66b5ae">
-              <animate attributeName="cx" values="25.729490168751575;59.27050983124842" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              <animate attributeName="cy" values="32.366442431225806;21.46830451114539" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              <animate attributeName="fill" values="#66b5ae;#5da4dc" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-            </circle><circle cx="59.27050983124842" cy="21.46830451114539" r="5" fill="#5da4dc">
-              <animate attributeName="cx" values="59.27050983124842;80" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              <animate attributeName="cy" values="21.46830451114539;49.99999999999999" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-              <animate attributeName="fill" values="#5da4dc;#d7689d" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animate>
-            </circle>
-            </svg>
             <ul>
               <li v-for="(file, i) in files " :key="i" :data-cy="file.id">
                 <div style="padding-bottom: 10px;">
